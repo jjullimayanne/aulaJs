5688c36f734f3babc6a3721efb2ee12b9be69ca5
Julli Mayanne <jullimayanne9@gmail.com>
2022-07-12 01:27:00 -0300

    refact: aula_um descritivo

--- src/aula_um.js
+++ src/aula_um.js
@@ -3,12 +3,14 @@
 // Devem ser impressas as seguintes mensagens:
 // - ACESSO PERMITIDO caso a senha seja válida.
 // - ACESSO NEGADO caso a senha seja inválida
+
+// siga boilerplate (modelo) para realizar o exercicio
+// a funçao validaSenha irá receber um argumento (senha)
 const permitido = "ACESSO PERMITIDO";
 const negado = "ACESSO NEGADO";
 
-function validaSenha(senha) {
-    
-//escreva a sua solucao
+function validaSenha(senha) {  
+//retorne aqui a sua solucao
 }
 
 module.exports = validaSenha;
