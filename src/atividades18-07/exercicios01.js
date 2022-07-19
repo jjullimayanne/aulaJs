a17093d625846a1f89d27d50429f5c4e64653834
ArleyOliveiraMendonca <arley@arley.com.br>
2022-07-18 22:01:22 -0300

    Segue o Ex 04

--- src/atividades18-07/exercicios01.js
+++ src/atividades18-07/exercicios01.js
@@ -1,12 +1,12 @@
 let rs = require('readline-sync');
 
-let numero1 = rs.questionInt("Digite o primeiro número: ");
-let numero2 = rs.questionInt("Digite o segundo número: ");
+let numero1 = rs.questionInt("Digite o primeiro numero: ");
+let numero2 = rs.questionInt("Digite o segundo numero: ");
 
 if(numero1 === numero2){
     console.log("Os números são iguais!")
 }else if(numero1 > numero2){
-    console.log("O primeiro número é maior")
+    console.log("O primeiro numero é maior")
 }else{
-    console.log("O segundo número é maior")
+    console.log("O segundo numero é maior")
 }
